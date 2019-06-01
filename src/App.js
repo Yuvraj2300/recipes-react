b25d4736e96fac5ed8b15fdbaf607055d49cc22f
yuvraj <yuvraj_2300@hotmail.com>
2019-06-01 18:04:51 +0530

    First Commit, added async func to skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { recipes } from './tempList';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    recipes: [],
+    url: "https://www.food2fork.com/api/search?key=7ceb253de53f24e234b0cce99d371aa3"
+  };
+
+  // AJAX CODE
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  //lifecycle mehods.....
+  componentDidMount() {
+    console.log("component mounted")
+    this.getRecipes()
+  }
+
+
+  render() {
+    //checking if the data recieved
+    console.log(this.state.recipes)
+    return (
+      <React.Fragment>
+        <RecipeList />
+
+        <RecipeDetails />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello From Recipe!</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Recipe;
